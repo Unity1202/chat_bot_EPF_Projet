3731878e7358aba318733e26f6bfc684f6955d84
alexis.dhermy <a.dhermy@live.com>
2025-06-06 18:35:59 +0200

    fix: taille historique

--- src/components/Sidebar/HistoryHeader.jsx
+++ src/components/Sidebar/HistoryHeader.jsx
@@ -5,7 +5,7 @@ const HistoryHeader = () => {
   return (
     <div className="flex items-center gap-1">
       <History className="h-3.5 w-3.5 text-[#16698C]" />
-      <h2 className="text-sm font-medium">Historique</h2>
+      <h2 className="text-l font-medium">Historique</h2>
     </div>
   );
 };
